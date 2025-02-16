e7801b2eaf606e463b8cd0e15652cb0c28f67733
unknown <n.t.sithy@gmail.com>
2025-02-16 11:44:50 +0600

    sithy33 has modified

--- primenumber.cpp
+++ primenumber.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 
 //write a function to print prime numbers from 1 to 100 and call it in the main function
+void printfunc(){
+    cout<<"printing func"<<endl;
+}
 
 int main() {
 
     //call the function here
+    printfunc();
     return 0;
 }
