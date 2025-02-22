d617b9b73326f878cf5432cb459a868b253577cb
lam0011 <lamiyaalif00@gmail.com>
2025-02-22 19:47:03 +0600

    function written for print prime numbers from 1 to 100

--- primenumber.cpp
+++ primenumber.cpp
@@ -1,7 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//write a function to print prime numbers from 1 to 100 and call it in the main function
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+void printPrimeNumbers() {
+    for (int i = 1; i <= 100; i++) {
+        if (isPrime(i)) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
 
 int main() {
 
